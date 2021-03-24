11d447eda562d93283eff01cd1e97446bf7e52b5
Kerem <karry@Kerems-MacBook-Pro.local>
2021-03-25 02:17:33 +0300

    newline added

--- generate_client_ips.py
+++ generate_client_ips.py
@@ -10,4 +10,4 @@ with open(gateways_interfaces_filepath, "r") as infile:
 
 with open("tun_ip_client.txt", "w") as outfile:
 	for i, gw_int_tuple in zip(range(client_count), gw_int):
-		outfile.write("tun{}:10.0.1.{}:{}:{}:{}:iotlab{}".format(i, (i+1)*2, gw_int_tuple[0], gw_int_tuple[1], (i+1)*2-1, i+1))
+		outfile.write("tun{}:10.0.1.{}:{}:{}:{}:iotlab{}\n".format(i, (i+1)*2, gw_int_tuple[0], gw_int_tuple[1], (i+1)*2-1, i+1))
